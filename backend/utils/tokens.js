57a5a80c0c9217ab7a3c15669de9219846e80056
Nikhil Srivastava <nikhilsrv2017@gmail.com>
2024-11-15 08:10:32 +0530

    updated token

--- backend/utils/tokens.js
+++ backend/utils/tokens.js
@@ -11,7 +11,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, 
 		httpOnly: true, 
 		sameSite: "strict", 
-		secure: false,
+		secure: true,
 	});
 };
 
